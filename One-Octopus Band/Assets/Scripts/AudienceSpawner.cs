e59bf77aa366e661cd17b8bf840c31f75094e3c1
Axel Nordanskog <axeno840@student.liu.se>
2019-11-09 16:58:59 +0100

    Light up concert

--- One-Octopus Band/Assets/Scripts/AudienceSpawner.cs
+++ One-Octopus Band/Assets/Scripts/AudienceSpawner.cs
@@ -16,21 +16,22 @@ public class AudienceSpawner : MonoBehaviour
 
 		while (!validPosition && spawnAttempts < maxSpawnAttempts)
 		{
+			print("Attempt " + spawnAttempts);
 			++spawnAttempts;
 			
 			spawnPos = new Vector3(
 				Random.Range(
-					transform.position.x - 0.5f * transform.localScale.x,
-					transform.position.x + 0.5f * transform.localScale.x),
-				0.0f,
+					transform.position.x - 0.5f * transform.lossyScale.x,
+					transform.position.x + 0.5f * transform.lossyScale.x),
+				transform.position.y,
 				Random.Range(
-					transform.position.z - 0.5f * transform.localScale.z,
-					transform.position.z + 0.5f * transform.localScale.z));
+					transform.position.z - 0.5f * transform.lossyScale.z,
+					transform.position.z + 0.5f * transform.lossyScale.z));
 			
 			validPosition = true;
 			foreach (Collider col in Physics.OverlapSphere(spawnPos, obstacleCheckRadius))
 			{
-				if (col.GetComponent<AudienceMember>())
+				if (col.GetComponentInParent<AudienceMember>())
 				{
 					validPosition = false;
 					break;
@@ -44,13 +45,14 @@ public class AudienceSpawner : MonoBehaviour
 		}
 		else
 		{
+			print("No more attempts, mate!");
 			return null;
 		}
 	}
 
 	public void OnDrawGizmos()
 	{
-		Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x, 0, transform.localScale.z));
+		Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x, 0, transform.lossyScale.z));
 	}
 
 	public void DrawGizmosMember(AudienceMember member)
