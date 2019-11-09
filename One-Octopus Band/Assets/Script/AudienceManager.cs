4674e50c15fe24113a2bc6610bd9b3def6c45903
Axel Nordanskog <axeno840@student.liu.se>
2019-11-09 10:13:10 +0100

    Add concert scene with spawning audience

---
+++ One-Octopus Band/Assets/Script/AudienceManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceManager : MonoBehaviour
+{
+	public int maxMemberCount;
+	private int currentMemberCount = 0;
+
+	private List<AudienceMember> members = new List<AudienceMember>();
+	private AudienceSpawner spawner;
+
+	void Start()
+	{
+		spawner = GetComponent<AudienceSpawner>();
+		while (currentMemberCount < maxMemberCount)
+		{
+			AudienceMember newMember = spawner.SpawnAudienceMember();
+			if (newMember != null)
+			{
+				++currentMemberCount;
+				members.Add(newMember);
+			}
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		foreach (var member in members)
+		{
+			spawner.DrawGizmosMember(member);
+		}
+	}
+}
---
+++ One-Octopus Band/Assets/Script/AudienceSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceSpawner : MonoBehaviour
+{
+	public int maxSpawnAttempts;
+	public float obstacleCheckRadius;
+	public AudienceMember audiencePrefab;
+
+	public AudienceMember SpawnAudienceMember()
+	{	
+		Vector3 spawnPos = Vector3.zero;
+		bool validPosition = false;
+		int spawnAttempts = 0;
+
+		while (!validPosition && spawnAttempts < maxSpawnAttempts)
+		{
+			++spawnAttempts;
+			
+			spawnPos = new Vector3(
+				Random.Range(
+					transform.position.x - 0.5f * transform.localScale.x,
+					transform.position.x + 0.5f * transform.localScale.x),
+				0.0f,
+				Random.Range(
+					transform.position.z - 0.5f * transform.localScale.z,
+					transform.position.z + 0.5f * transform.localScale.z));
+			
+			validPosition = true;
+			foreach (Collider col in Physics.OverlapSphere(spawnPos, obstacleCheckRadius))
+			{
+				if (col.GetComponent<AudienceMember>())
+				{
+					validPosition = false;
+					break;
+				}
+			}
+		}
+		
+		if (validPosition)
+		{
+			return Instantiate(audiencePrefab, spawnPos, Quaternion.identity);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x, 0, transform.localScale.z));
+	}
+
+	public void DrawGizmosMember(AudienceMember member)
+	{
+		Gizmos.DrawWireSphere(member.transform.position, obstacleCheckRadius);
+	}
+}
