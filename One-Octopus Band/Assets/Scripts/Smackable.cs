50a052093a019023212d5a59f7c1e8dabf530d60
jakkarn <jakobnorell@gmail.com>
2019-11-09 19:34:17 +0100

    Spela shoreline! På triangle, with a schtick... fast med bara en trumma

--- One-Octopus Band/Assets/Scripts/Smackable.cs
+++ One-Octopus Band/Assets/Scripts/Smackable.cs
@@ -6,19 +6,26 @@ using System.Collections;
 public class Smackable : MonoBehaviour
 {
     public UnityEvent OnSmacked;
+    public AudioSource audio;
+    public AudioClip clip;
 
     void Update()
     {
-        OnSmacked.Invoke();
+        //OnSmacked.Invoke();
     }
 
     void OnTriggerEnter(Collider other)
     {
         
         var tentacle = other.gameObject.GetComponent<Tentacle>();
+
+        audio.clip = clip;
+        audio.Play();
+
         Debug.Log("this runs");
         if (tentacle != null)
         {
+           
             OnSmacked.Invoke();
         }
     }
