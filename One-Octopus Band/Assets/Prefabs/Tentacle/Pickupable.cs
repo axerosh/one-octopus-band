066b2a0c68fe09a5a0fec60e31e98cb08e82a3af
Erik <e.naaru@hotmail.com>
2019-11-10 12:33:31 +0100

    Add generalized Pickupable script that makes rigidbodies pickupable by the MEGA TENTACLE

---
+++ One-Octopus Band/Assets/Prefabs/Tentacle/Pickupable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour {
+    public Rigidbody body;
+    private Transform holder = null;
+
+    private void FixedUpdate() {
+        if(holder != null) {
+            body.MovePosition(holder.position);
+        }
+    }
+
+    public void Pickup(Pickuper picker) {
+        holder = picker.transform;
+        body.useGravity = false;
+    }
+
+    public void Release() {
+        holder = null;
+        body.useGravity = true;
+    }
+
+}
---
+++ One-Octopus Band/Assets/Prefabs/Tentacle/Pickuper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickuper : MonoBehaviour {
+    private bool pickupMode = false;
+
+    private List<Pickupable> pickedStuff = new List<Pickupable>();
+
+    private void OnTriggerEnter(Collider other) {
+        if (pickupMode) {
+            Pickupable pickup = other.gameObject.GetComponent<Pickupable>();
+            if(pickup != null) {
+                pickup.Pickup(this);
+                pickedStuff.Add(pickup);
+            }
+        }
+    }
+
+    public void StartPickUp() {
+        pickupMode = true;
+    }
+
+    public void Release() {
+        pickupMode = false;
+        foreach(Pickupable p in pickedStuff) {
+            p.Release();
+        }
+        pickedStuff.Clear();
+    }
+}
